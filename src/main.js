7036d0801d2bceff899bacad28566e6e1d6364b2
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-06-22 13:59:34 -0700

    adds UI logic for soil values
    Co-authored-by: Thomas Russell <t.p.russell19@gmail.com>

--- src/main.js
+++ src/main.js
@@ -5,5 +5,13 @@ import './css/styles.css';
 import './js/service-logic.js';
 
 $(document).ready(function() {
-  
-})
+  $('#feed').click(function() {
+    const newState = stateControl(blueFood);
+    $('soil-value').text(`Soil: ${newState.soil}`);
+  });
+
+  $('#show-state').click(function() {
+    const currentState = stateControl();
+    $('#soil-value').text(`Soil: ${currentState.soil}`);
+  });
+});
