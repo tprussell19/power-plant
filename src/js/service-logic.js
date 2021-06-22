d1d287b227958806babfa2e21e65b12031d918b6
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-06-22 13:56:18 -0700

    adds feed and hydrate functions
    Co-authored-by: Thomas Russell <t.p.russell19@gmail.com>

--- src/js/service-logic.js
+++ src/js/service-logic.js
@@ -1,30 +1,30 @@
-export const hydrate = (plant) => {
-  return {
-    ...plant,
-    water: (plant.water || 0) + 1
-  }
-};
+// export const hydrate = (plant) => {
+//   return {
+//     ...plant,
+//     water: (plant.water || 0) + 1
+//   }
+// };
 
-export const feed = (plant) => {
-  return {
-    ...plant,
-    soil: (plant.soil || 0) + 1
-  }
-};
+// export const feed = (plant) => {
+//   return {
+//     ...plant,
+//     soil: (plant.soil || 0) + 1
+//   }
+// };
 
-export const giveLight = (plant) => {
-  return {
-    ...plant,
-    light: (plant.light || 0) + 1
-  }
-};
+// export const giveLight = (plant) => {
+//   return {
+//     ...plant,
+//     light: (plant.light || 0) + 1
+//   }
+// };
 
-export const changePlantState = (plant, prop) => {
-  return {
-    ...plant,
-    [prop]: (plant[prop] || 0) + 1
-  }
-}
+// export const changePlantState = (plant, prop) => {
+//   return {
+//     ...plant,
+//     [prop]: (plant[prop] || 0) + 1
+//   }
+// }
 
 export const changeState = (prop) => {
   return (val) => {
@@ -45,3 +45,9 @@ export const storeState = () => {
 }
 
 export const stateControl = storeState();
+
+export const feed = changeState("soil")(1);
+export const blueFood = changeState("soil")(5);
+
+export const hydrate = changeState("water")(1);
+export const superWater = changeState("water")(5);
